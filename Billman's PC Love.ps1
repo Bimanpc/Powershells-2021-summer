d1f9fe1cf0b14934e3a6de950f43592eaf8ea380
Βασίλης Ψαρομάτης PC TECH GR EU BIILMAN <77292453+Bimanpc@users.noreply.github.com>
2021-08-15 19:40:37 +0300

    Add files via upload

---
+++ Billman's PC Love.ps1
@@ -0,0 +1,13 @@
+﻿Add-Type -assembly System.Windows.Forms
+$main_form = New-Object System.Windows.Forms.Form
+$main_form.Text ='GUI Billman s PC TECH Lovers'
+$main_form.Width = 600
+$main_form.Height = 400
+$main_form.AutoSize = $true
+Now you can display the form on the screen.
+$Label = New-Object System.Windows.Forms.Label
+$Label.Text = "I LOVE PC AND TECH"
+$Label.Location  = New-Object System.Drawing.Point(0,10)
+$Label.AutoSize = $true
+$main_form.Controls.Add($Label)
+$main_form.ShowDialog()
